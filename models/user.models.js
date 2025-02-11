fafdac718def7f75866f3eef9c4ecb5cb3b58fe1
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2025-02-11 23:26:45 +0530

    user model

--- models/user.models.js
+++ models/user.models.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 1 import mongoose
 const mongoose = require("mongoose");
 
@@ -38,37 +37,4 @@ const userSchema = new mongoose.Schema(
 // export model
 const Users = mongoose.model("Users", userSchema);
 module.exports = Users;
-=======
-const mongoose= require("mongoose")
 
-const user={
-    username:{
-        type:string,
-        required:true,
-        unique:true
-    },
-    email:{
-        type:string,
-        required:true,
-        unique:true,
-        lowercase:[true,"email should be in lowercase"]
-    },
-    password:{
-        type:string,
-        required:true,
-        min:[6,"Must be atleast 6, got {value}"],
-        max:12
-    }
-    ,
-    confirmPassword:{
-        type:string,
-        required:true,
-        min:[6,"Must be atleast 6, got {value}"],
-        max:12
-    }
-}
-
-const userModel= mongoose.model("userModel",user)
-module.exports=userModel
-
->>>>>>> 035174deb196e6d44e7c09b5791c50f8ebb9ee44
