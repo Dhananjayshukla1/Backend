8b62d893137c280bed500c83973509c57b9ecaec
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2025-02-14 11:48:07 +0530

    "fix: confidential data removed"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 // console.log(process.env)
 const mongoose = require("mongoose");
 const app = express();
@@ -9,19 +9,18 @@ const app = express();
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is started "+ `${process.env.PORT}`);
+  console.log("Server is started " + `${process.env.PORT}`);
 });
 
-
-app.use("/api/users",()=>{})
+app.use("/api/users", () => {});
 
 // database connection
-
 mongoose
-  .connect("mongodb+srv://dhananjayshukla199:rcUvL0yQwSoJxe4j@cluster1.q0rg9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
-  .then(function (db) {
-    console.log("DB is connected");
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("DB is connected"))
+  .catch((err) => console.log(err));
+
+
